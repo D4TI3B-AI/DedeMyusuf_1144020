9274385e9ce17f3f4f59d567afa0c23314578ff0
Dedemyusuf <dedemyusuf96@gmail.com>
2017-03-27 20:53:49 +0700

    Add files via upload

--- pertemuan3/pertemuan3.py
+++ pertemuan3/pertemuan3.py
@@ -1,1 +1,23 @@
-
+print("game plesples")
+print("pilih domba/sayur/serigala")
+a = raw_input('pilih ?')
+if a == "domba":
+    b = raw_input('nice, pilih? ')
+    if b == "sayur":
+        c = raw_input('nice, pilih? ')
+        if c == "domba":
+            d = raw_input('nice, pilih? ')
+            if d == "serigala":
+                e = raw_input('nice, pilih? ')
+                if e == "domba":
+                    print("nice, you win")
+                else:
+                    print('noob')
+            else:
+                print('noob')
+        else:
+            print('noob')
+    else:
+        print('noob')
+else:
+    print('noob')
